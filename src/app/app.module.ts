06116fc97a20a35476fad66eb08aeb686425523e
MahdisKalate <kl.mahdis64@gmail.com>
2022-03-06 12:35:00 +0330

    with guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { SecondScreenComponent } from './second-screen/second-screen.component';
 import { Routes , RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from './guard/auth.guard';
 
 const appRoutes: Routes = [
   {path : '', redirectTo: '/first', pathMatch: 'full'},
   {path: 'first', component: HomeComponent },
   {path: 'second', component: SecondScreenComponent},
-  {path: 'profile/:phonenumber', component: ProfileComponent},
+  {path: 'profile/:phonenumber', component: ProfileComponent , canActivate:[AuthGuard] },
   {path: '**', component: PageNotFoundComponent}
   
 ];
